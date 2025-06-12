3660d418934a381b05db6e1bff41828678a69527
Rambo12321 <rohankaushik2015@gmail.com>
2025-06-12 21:13:05 +0530

    File changes, routing setup dashboard and UI

---
+++ app/routes/admin/dashboard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const dashboard = () => {
+  return (
+    <div className="text-amber-950 text-3xl font-bold">dashboard Page</div>
+  );
+};
+
+export default dashboard;
